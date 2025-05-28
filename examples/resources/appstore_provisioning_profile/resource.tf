4d1b215995da4a6581d99d213af6ced3ecd2b291
Adam Kovacs <kovadam98@gmail.com>
2025-05-28 12:34:08 +0200

    fix: docs

--- examples/resources/appstore_provisioning_profile/resource.tf
+++ examples/resources/appstore_provisioning_profile/resource.tf
@@ -1,6 +1,13 @@
+resource "appstore_bundle_identifier" "this" {
+  name       = "Terraform Test Bundle Identifier"
+  identifier = "com.terraform.test"
+  platform   = "UNIVERSAL"
+  capabilities = ["APPLE_ID_AUTH"]
+}
+
 resource "appstore_provisioning_profile" "this" {
   name       = "Terraform Test Provisioning Profile"
-  bundle_identifier_id = "..."
+  bundle_identifier_id = appstore_bundle_identifier.this.id
   type = "IOS_APP_STORE"
   certificate_ids = ["..."]
 }
